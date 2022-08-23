c6c18640a08b70146e41bed9245f483c280154e5
lupusalba <a145296@gmail.com>
2022-08-24 00:09:48 +0200

    changed ui, 80% done

--- src/App.js
+++ src/App.js
@@ -1,322 +1,155 @@
-import { useState, useEffect } from "react"
-import './App.css';
+import { VirtualType } from "mongoose";
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-
-  // const remainder = "https://ibaslogic.com/simple-guide-to-react-form/ dodati ovaj oblik prigaza podataka u sekciju za iskustvo i edukaciju, srediti style iz FIGMA fajla"
-  // console.log(remainder);
-  // const [formData, setFormData] = useState(
-  //   () => JSON.parse(localStorage.getItem(""))//formData
-  //     ||
-  //   {
-  //     firstName: "Ime",
-  //     lastName: "Prezime",
-  //     hasPracticalExpirience: false,
-  //     bio: "Biografija biografija, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. ",
-  //     contact: "+000 111 2222 323, e@mail.com",
-  //     education: ["education, education, Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. "],
-  //     expirience: ["expirience, expirience, expirience"],
-  //     employment: "",
-  //     position: "",
-  //     industryKnowledge: "industy, Knowlage",
-  //     toolsAndTechnologies: "tools, and, tehnologies",
-  //     otherSkills: "other, skills",
-  //     languages: "llanguages, languages",
-  //     social: "social, skills",
-  //   }
-  // )
-
-  // // useEffect( () => {
-  // //   localStorage.setItem("formData", JSON.stringify(formData))
-  // // }, [formData])
-
-
-  //   const [anotherItem, setAnotherItem] = useState("")
-  // // add another item
-  // const addAnotherItemToTheList = (AnotherItem) => {
-  //   const id = Math.floor(Math.random() * 10000) + 1
-  //   const newItem = { id, ...AnotherItem }
-  //   setAnotherItem ([...AnotherItem], newItem)
-  // }
-
-  // // delete the existing item
-  // const deleteItemFromTheList = (id) => {
-  //   setAnotherItem()
-  // }
-
-
-  function handleChange(event) {
-
-    const { name, value, type, checked } = event.target
-
-    setFormData(prevFormData => {
-      return {
-        ...prevFormData,
-        [name]: type === "checkbox" ? checked : value
-
-      }
-    })
-  }
-
-
-
-  function handleSubmit(event) {
-    event.preventDefault()
-    console.log(formData)
-    writeFormData(formData)
-  }
-
-  const writeFormData = (data) => {
-    const t = data.firstName
-    console.log(t)
-    return (
-      <p>{data.firstName}</p>
-    )
-  }
-
-
-
   return (
     <div className="App">
+      <div className="cvWrapper">
+        <div className="grid">
+          <div id="main" className="gridItem">
+            <div className="sectionContainer">
+              <div id="profileImage">
+                <img src="https://images.pexels.com/photos/1040881/pexels-photo-1040881.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" />
+              </div>
 
-      <div className="cvContainer">
-        {/* <div className="inData">
-          <form onSubmit={handleSubmit}>
+              <div id="jobTitle">Web Developer</div>
 
-            <input
-              className="inputFull"
-              type="text"
-              placeholder="First Name"
-              name="firstName"
-              onChange={handleChange}
-              value={formData.firstName}
-            />
+              <div id="userDescription"  className="listItemFontSize">
+                lorem ipsum dolor sit lorem ipsum dolor lorem ipsum dolor lorem
+                ipsum dolor lorem ipsum dolor lorem sit.
+              </div>
 
+              <div id="skills"  className="listItemFontSize">
+                <p className="title">skills</p>
 
-            <input
-              className="inputFull"
-              type="text"
-              placeholder="Last Name"
-              name="lastName"
-              onChange={handleChange}
-              value={formData.lastName}
-            />
+                <div className="skillWrapper">
+                  <div className="skillName">Skill name</div>
+                  <div className="percentContainer">
+                    <div className="skill skill1">90%</div>
+                  </div>
+                </div>
+                <div className="skillWrapper">
+                  <div className="skillName">Skill name</div>
+                  <div className="percentContainer">
+                    <div className="skill skill2">70%</div>
+                  </div>
+                </div>
+                <div className="skillWrapper">
+                  <div className="skillName">Skill name</div>
+                  <div className="percentContainer">
+                    <div className="skill skill3">60%</div>
+                  </div>
+                </div>
+                <div className="skillWrapper">
+                  <div className="skillName">Skill name</div>
+                  <div className="percentContainer">
+                    <div className="skill skill4">40%</div>
+                  </div>
+                </div>
+                <div className="skillWrapper">
+                  <div className="skillName">Skill name</div>
+                  <div className="percentContainer">
+                    <div className="skill skill5">30%</div>
+                  </div>
+                </div>
 
-            <label htmlFor="hasPracticalExpirience">Practical expirience</label>
-            <input
-              className="checkbox"
-              type="checkbox"
-              id="hasPracticalExpirience"
-              name="hasPracticalExpirience"
-              onChange={handleChange}
-              checked={formData.hasPracticalExpirience}
-            />
+              </div>
 
-            <textarea
-              className="textarea"
-              placeholder="Short biography"
-              name="bio"
-              onChange={handleChange}
-              value={formData.bio}
-            />
+              <div className="work">
+                <p className="title">Work</p>
+                <div className="workPosition">
+                  <div className="timeLong">present</div>
+                  <div className="company">company</div>
+                  <div className="positionTitle">positionTitle</div>
+                  <div className="positionDescription">lorem ipsum dolor sit lorem ipsum dolor</div>
+                </div>
 
-            <input
-              className="inputFull"
-              type="text"
-              placeholder="Contact"
-              name="contact"
-              onChange={handleChange}
-              value={formData.contact}
-            />
-            <button className="btnAdd">Add</button>
+                <div className="workPosition">
+                  <div className="timeLong">present</div>
+                  <div className="company">company</div>
+                  <div className="positionTitle">positionTitle</div>
+                  <div className="positionDescription">lorem ipsum dolor sit lorem ipsum dolor</div>
+                </div>
 
-
-            <input
-              className="inputFull"
-              type="text"
-              placeholder="Education"
-              name="education"
-              onChange={handleChange}
-              value={formData.education}
-            />
-            <button className="btnAdd">Add</button>
-
-            <input
-              className="inputFull"
-              type="text"
-              placeholder="Experience"
-              name="expirience"
-              onChange={handleChange}
-              value={formData.expirience}
-            />
-            <button className="btnAdd">Add</button>
-
-            <br />
-            <br />
-
-            <fieldset>
-              <legend>Current employment status</legend>
-
-              <input
-                type="radio"
-                id="unemployed"
-                name="employment"
-                value="unemployed"
-                checked={formData.employment === "unemployed"}
-                onChange={handleChange}
-              />
-              <label htmlFor="unemployed">unemployed</label>
-
-              <input
-                type="radio"
-                id="employed"
-                name="employment"
-                value="employed"
-                checked={formData.employment === "employed"}
-                onChange={handleChange}
-              />
-              <label htmlFor="employed">employed</label>
-
-              <input
-                type="radio"
-                id="part-time"
-                name="employment"
-                value="part-time"
-                checked={formData.employment === "part-time"}
-                onChange={handleChange}
-              />
-              <label htmlFor="part-time">part-time</label>
-
-            </fieldset>
-
-            <label htmlFor="Position">Position</label>
-            <select
-              id="position"
-              value={formData.position}
-              onChange={handleChange}
-              name="position"
-            >
-              <option value="">-- Chose --</option>
-              <option value="Junior">Junior</option>
-              <option value="Medior">Medior</option>
-              <option value="Senior">Senior</option>
-            </select>
-
-            <br />
-            <label htmlFor="industryKnowlage">Industry Knowlage</label>
-            <input
-              className="inputFull"
-              id="industryKnowlage"
-              value={formData.industryKnowlage}
-              onChange={handleChange}
-              name="industryKnowlage"
-            />
-            <button className="btnAdd">Add</button>
-
-            <label htmlFor="toolsAndTechnologies">toolsAndTechnologies</label>
-            <input
-              className="inputFull"
-              id="toolsAndTechnologies"
-              value={formData.toolsAndTechnologies}
-              onChange={handleChange}
-              name="toolsAndTechnologies"
-            />
-            <button className="btnAdd">Add</button>
-
-            <label htmlFor="otherSkills">otherSkills</label>
-            <input
-              className="inputFull"
-              id="otherSkills"
-              value={formData.otherSkills}
-              onChange={handleChange}
-              name="otherSkills"
-            />
-            <button className="btnAdd">Add</button>
-
-            <label htmlFor="languages">languages</label>
-            <input
-              className="inputFull"
-              id="languages"
-              value={formData.languages}
-              onChange={handleChange}
-              name="languages"
-            />
-            <button className="btnAdd">Add</button>
-
-            <label htmlFor="social">social</label>
-            <input
-              className="inputFull"
-              id="social"
-              value={formData.social}
-              onChange={handleChange}
-              name="social"
-            />
-            <button className="btnAdd">Add</button>
-
-            <button type="submit">Submit data</button>
-          </form>
-        </div> */}
-
-        <div className="outData">
-
-          <div className="border header">
-            <div className="userFirstName">{formData.firstName}</div>
-            <div className="userLastName">{formData.lastName}</div>
-            <div className="userTitle">{formData.title}</div>
-            <div className="userExp">pomeri{formData.expirience ? "Yes" : "No"}</div>
-            <div className="userEmployment">pomeri{formData.employment}</div>
+                <div className="workPosition">
+                  <div className="timeLong">present</div>
+                  <div className="company">company</div>
+                  <div className="positionTitle">positionTitle</div>
+                  <div className="positionDescription">lorem ipsum dolor sit lorem ipsum dolor</div>
+                </div>
+              </div>
+            </div>
           </div>
+          <div id="aside" className="gridItem">
+            <div className="basicInfo subSection">
+              <ul className="listItemFontSize">
+                <li>date</li>
+                <li>mail</li>
+                <li>phone</li>
+                <li>address</li>
+                <li>website</li>
+              </ul>
+            </div>
 
-          <div className="cvMainContent">
+            <div className="social">
+              <i>icon</i>
+              <i>icon</i>
+              <i>icon</i>
+            </div>
 
-            <div className="border expAndBio">
-              <div className="userBio">{formData.bio}</div>
-              <div className="horizontalDivider"></div>
-              <div className="userEducation">{formData.education}</div>
-            </div>{/* expAndBio*/}
+            <div className="education subSection">
+              <p className="title">graduation</p>
+              <p className="description">lorem ipsum dolor</p>
+            </div>
 
-            <div className="border aside">
+            <div className="personalValues subSection">
+              <p className="title">personalValues</p>
+              <ul className="listItemFontSize">
+                <li>value</li>
+                <li>value</li>
+                <li>value</li>
+                <li>value</li>
+                <li>value</li>
+                <li>value</li>
+              </ul>
+            </div>
 
-              <div className="asideItem userContact">
+            <div className="expertise subSection">
+              <p className="title">expertise</p>
 
-                <span className="asideItemElement">{
-                  formData.contact
-                }
-                </span>
-              </div>
+              <ul className="listItemFontSize">
+                <li>expertise</li>
+                <li>expertise</li>
+                <li>expertise</li>
+                <li>expertise</li>
+                <li>expertise</li>
+                <li>expertise</li>
+              </ul>
+            </div>
 
+            <div className="languages subSection">
+              <p className="title">languages</p>
+              <ul className="listItemFontSize">
+                <li>languages</li>
+                <li>languages</li>
+                <li>languages</li>
+                <li>languages</li>
+                <li>languages</li>
+                <li>languages</li>
+              </ul>
+            </div>
 
-
-              <div className="asideItem userIndustryKnowlage">Title ----
-                <span className="asideItemElement">{formData.industryKnowledge}</span>
-              </div>
-
-              <div className="asideItem userToolsAndTechnologies">Title ----
-                <span className="asideItemElement">{formData.toolsAndTechnologies}</span>
-              </div>
-
-              <div className="asideItem userOtherSkills">Title ----
-                <span className="asideItemElement">{formData.otherSkills}</span>
-              </div>
-
-              <div className="asideItem userLanguages">Title ----
-                <span className="asideItemElement">{formData.languages}</span>
-              </div>
-
-              <div className="asideItem userSocial">Title ----
-                <span className="asideItemElement">
-                  {formData.social}</span>
-              </div>
-
-              <div className="userPosition">promeni{formData.position}</div>
-            </div>{/*.aside */}
-          </div>{/* .cvData */}
-
-        </div>{/* .outData*/}
-
-
-      </div>{/* cvContainer */}
-
+            <div className="interests subSection">
+              <p className="title">interests</p>
+              <i>icon</i>
+              <i>icon</i>
+              <i>icon</i>
+              <i>icon</i>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
